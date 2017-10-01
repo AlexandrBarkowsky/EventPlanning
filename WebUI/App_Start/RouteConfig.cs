4272bbaabf91e8113a8d0063c746666aefb9a498
Alexandr Barkowsky <safoktv@gmail.com>
2017-10-01 21:51:43 +0300

    Renamed default router

--- WebUI/App_Start/RouteConfig.cs
+++ WebUI/App_Start/RouteConfig.cs
@@ -22,7 +22,7 @@ namespace WebUI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Event", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
